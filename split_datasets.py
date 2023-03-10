3ad2e5e98374d27d74cc9e8bb2f328cafd1d691d
Michelle Qin <qin.michelle24@gmail.com>
2023-03-09 21:23:33 -0800

    remove hyphens from dates

--- split_datasets.py
+++ split_datasets.py
@@ -1,31 +1,38 @@
 from tqdm import tqdm
 
+def convert_date(line):
+    date = line[0:10].replace('-', '')
+    new_line = date
+    for i in range(10, len(line)):
+        new_line += line[i]
+    return new_line
+
 def get_training_dataset(data_2021_path, data_2022_path):
     with open('training_dataset.csv', 'w') as f:
         data_2021 = open(data_2021_path, 'r').readlines()
         data_2022 = open(data_2022_path, 'r').readlines()
 
         for line in tqdm(data_2021):
-            f.write(line)
-
+            f.write(convert_date(line))
+        f.write('\n')
         for line in tqdm(data_2022[1:len(data_2022)//2]):
-            f.write(line)
+            f.write(convert_date(line))
 
 def get_validation_dataset(data_2022_path):
     with open('validation_dataset.csv', 'w') as f:
         data_2022 = open(data_2022_path, 'r').readlines()
 
         for line in tqdm(data_2022[len(data_2022)//2:]):
-            f.write(line)
+            f.write(convert_date(line))
 
 def get_testing_dataset(data_2023_path):
     with open('testing_dataset.csv', 'w') as f:
         data_2023 = open(data_2023_path, 'r').readlines()
 
         for line in tqdm(data_2023):
-            f.write(line)
+            f.write(convert_date(line))
 
 if __name__ == '__main__':
     get_training_dataset('us-counties-2021.csv', 'us-counties-2022.csv')
-    # get_validation_dataset('us-counties-2022.csv')
-    # get_testing_dataset('us-counties-2023.csv')
+    get_validation_dataset('us-counties-2022.csv')
+    get_testing_dataset('us-counties-2023.csv')
